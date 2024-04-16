a752e6d35d06a870404471880d169d24cb2d5c36
Wajiha Khan <jiya.r.kh@gmail.com>
2024-04-16 19:17:07 -0400

    fixed error in translator api

--- routes/translateRoutes.js
+++ routes/translateRoutes.js
@@ -3,14 +3,15 @@ const { translateText } = require('../services/translateServices');
 const router = express.Router();
 
 router.post('/translate', async (req, res) => {
-  const { text, fromLang, toLang } = req.body;
-  console.log('text, fromLang, toLang', text, fromLang, toLang);
-  if (!text || !fromLang || !toLang) {
+  const { text, toLang } = req.query;
+  // const { fromLang } = req.query || null;
+  // console.log('text, toLang', text, toLang);
+  if (!text || !toLang) {
     return res.status(400).send({ error: 'Missing text or language information.' });
   }
 
   try {
-    const { translatedText, fromCache } = await translateText(text, fromLang, toLang);
+    const { translatedText, fromCache } = await translateText(text, toLang);
     res.send({ translatedText, fromCache });
   } catch (error) {
     res.status(500).send({ error: 'Failed to process translation.', details: error.message });
